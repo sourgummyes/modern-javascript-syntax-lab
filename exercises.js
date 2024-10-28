d1057c9bdd6ab40fc83af6d6a81ddbad112258b2
SourGummyes <dewet.co@gmail.com>
2024-10-28 12:00:58 -0400

    4 i think

--- exercises.js
+++ exercises.js
@@ -8,3 +8,30 @@ const mulNum = nums.map( (num) => {
     return num * 2
 })
 console.log(mulNum)
+
+console.log("###########")
+const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+const [firstTop, secondTop, ThirdTop] = pizzaToppings
+
+console.log(firstTop, secondTop, ThirdTop)
+
+console.log("###########")
+const car = {
+    make: 'Audi',
+    model: 'q5',
+  };
+
+const {make, model} = car
+console.log(make, model)
+
+console.log("###########")
+const controversialPizzaToppings = [...pizzaToppings]
+console.log(controversialPizzaToppings)
+
+console.log("###########")
+let propertyName = "username"
+const userProfile = {
+    [propertyName]: "test",
+}
+
+console.log("###########")
